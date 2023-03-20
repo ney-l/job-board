6204f95702e41b5849047033b3cb57043f5add04
Ney Lanjewar <neylanjewar@gmail.com>
2023-03-20 12:44:28 +0100

    Adds a component for SEO tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import type { NextPage } from 'next';
 import { Button } from '@/components/button';
 import { InputField } from '@/components/form';
 import { Link } from '@/components/link';
+import { Seo } from '@/components/seo/seo';
 
 const Home: NextPage = () => {
   return (
     <>
+      <Seo title="Jobs Board" />
       <Button variant="solid" type="button">
         Click Me
       </Button>
