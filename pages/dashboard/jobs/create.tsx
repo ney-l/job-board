b82c6b95e7d725f68616bda049249469e953a413
Ney Lanjewar <neylanjewar@gmail.com>
2023-04-05 12:43:37 +0200

    adds state management with zustand; notifies user on Job creation

--- pages/dashboard/jobs/create.tsx
+++ pages/dashboard/jobs/create.tsx
@@ -5,10 +5,21 @@ import { ReactElement } from 'react';
 import { Seo } from '@/components/seo';
 import { CreateJobForm } from '@/features/jobs';
 import { DashboardLayout } from '@/layouts/dashboard-layout';
+import { useNotifications } from '@/stores/notifications';
 
 const DashboardCreateJobPage = () => {
+  const { showNotification } = useNotifications();
+
   const router = useRouter();
-  const onSuccess = () => router.push(`/dashboard/jobs`);
+  const onSuccess = () => {
+    showNotification({
+      type: 'success',
+      title: 'Success 🎉',
+      duration: 5000,
+      message: 'Job created! ',
+    });
+    router.push(`/dashboard/jobs`);
+  };
   return (
     <>
       <Seo title="Create Job" />
